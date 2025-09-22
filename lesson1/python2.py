1a5c8dc99229dcb345a6f6cfbe862f2011ce9aed
sofiadmytriieva-dev <sofia.dmytriieva@gmail.com>
2025-09-22 17:20:24 +0300

    lesson1

--- lesson1/python2.py
+++ lesson1/python2.py
@@ -1,4 +1,6 @@
+#Знайти обернене число для заданого користувачем числа (x) (перевірити,
+#чи (x neq 0)).
 x = int(input(""))
 d = 1/x
-bool = (x, neq 0)
-print(d, bool)
+bool_value = (x != 0)
+print(d, bool_value)
--- lesson1/python3.py
+++ lesson1/python3.py
@@ -1,3 +1,4 @@
+#Вивести всі двоцифрові числа, кратні 9.
 for number in range(10, 100):
     if number % 9 == 0:
         print(number)
