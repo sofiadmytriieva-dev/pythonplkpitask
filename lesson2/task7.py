69ed2d1a84daea1f8527c6f69077bed05b7cafad
sofiadmytriieva-dev <sofia.dmytriieva@gmail.com>
2026-04-13 12:24:37 +0300

    Delete lesson2 directory

--- lesson2/task7.py
+++
@@ -1,9 +0,0 @@
-#Знайти максимальний елемент масиву із 20 випадкових чисел.
-import random
-
-a = []
-for i in range(20):
-    a.append(random.randint(-100, 100))
-
-print("Масив:", a)
-print("Максимальний елемент:", max(a))
